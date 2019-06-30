5828f783d61a0927ba9bbc92711287bb6cb143eb
UH <554c46@gmail.com>
2019-06-30 21:06:01 +0200

    find features with low correlations

--- binsel/__init__.py
+++ binsel/__init__.py
@@ -1,3 +1,5 @@
 from .negate_bool_features import negate_bool_features
 from .hard_voting import hard_voting
 from .enumerate_negations import enumerate_negations
+from .bootstrap_solutions import (
+    bootstrap_solutions_all, bootstrap_solutions_pre)
---
+++ binsel/bootstrap_solutions.py
@@ -0,0 +1,63 @@
+import numpy as np
+from korr import bootcorr, mcc, mincorr
+
+
+def bootstrap_solutions_all(X, n_select=5, max_rho=0.4, n_draws=50,
+                            subsample=0.7, replace=False, random_state=42,
+                            unique=True):
+    # compute all Matthew's correlations between (Xi,Xj)
+    rho3, _ = bootcorr(X, n_draws=n_draws, subsample=subsample,
+                       replace=replace, random_state=random_state,
+                       corr_fn=mcc)
+
+    # for each draw find the lowest abs(corr(Xi,Xj))
+    solutions = []   # store results in list
+    for cmat in rho3:
+        idxlo = mincorr(cmat, n_stop=n_select, max_rho=max_rho)
+        solutions.append(sorted(idxlo))
+
+    # done
+    if unique:
+        return np.unique(solutions, axis=0)
+    else:
+        return solutions
+
+
+def bootstrap_solutions_pre(X, y, n_select=5, max_rho=0.4, preselect=0.8,
+                            n_draws=50, subsample=0.7, replace=False,
+                            random_state=42, unique=True):
+    # convert to number of features to preselect
+    n_features = len(X[0])
+    if isinstance(preselect, float):
+        n_pre = max(1, int(n_features * min(1.0, preselect)))
+    elif isinstance(preselect, int):
+        n_pre = max(1, min(n_features, preselect))
+    else:
+        n_pre = None  # all
+
+    # compute all Matthew's correlations between (y,X) and (Xi,Xj)
+    rho3, _ = bootcorr(np.c_[y, X], n_draws=n_draws, subsample=subsample,
+                       replace=replace, random_state=random_state,
+                       corr_fn=mcc)
+
+    # for each draw find the lowest abs(corr(Xi,Xj))
+    solutions = []   # store results in list
+    for cmat in rho3:
+        # a) sort abs corr(y,x), b) sort by biggest, c) add +1 to index bc y
+        idxft = np.flip(np.argsort(np.abs(cmat[0, 1:]))) + 1
+        idxft = idxft[:n_pre]
+        # find the smallest corr(xi,xj) between selected features
+        idxlo = mincorr(
+            cmat[:, idxft][idxft, :],
+            n_stop=n_select,
+            max_rho=max_rho)
+        # convert indicies back to 'X' matrix (not 'dat')
+        idxlo = list(np.array(idxft[idxlo]) - 1)
+        # save it
+        solutions.append(sorted(idxlo))
+
+    # done
+    if unique:
+        return np.unique(solutions, axis=0)
+    else:
+        return solutions
