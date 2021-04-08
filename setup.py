b929e12d726003590bb94cdc653019e396edf3ea
UH <554c46@gmail.com>
2021-04-08 11:19:33 +0200

    rst for pypi, zip file install

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 from setuptools import setup
-
-
-def read(fname):
-    import os
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+import pypandoc
 
 
 setup(name='binsel',
       version='0.2.2',
       description='Feature selection for Hard Voting classifier',
-      long_description=read('README.md'),
-      long_description_content_type='text/markdown',
+      long_description=pypandoc.convert('README.md', 'rst'),
       url='http://github.com/kmedian/binsel',
       author='Ulf Hamster',
       author_email='554c46@gmail.com',
@@ -23,4 +18,4 @@ setup(name='binsel',
           'scikit-learn>=0.20.0'
       ],
       python_requires='>=3.6',
-      zip_safe=False)
+      zip_safe=True)
